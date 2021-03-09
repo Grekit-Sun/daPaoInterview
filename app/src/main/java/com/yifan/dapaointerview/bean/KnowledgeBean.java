ec7e1f794256c7c7786b73c912cc5cab7dc89328
sun <grekit_sun@163.com>
2021-03-10 00:45:46 +0800

    databinding init

--- app/src/main/java/com/yifan/dapaointerview/bean/KnowledgeBean.java
+++ app/src/main/java/com/yifan/dapaointerview/bean/KnowledgeBean.java
@@ -7,6 +7,86 @@ package com.yifan.dapaointerview.bean;
  */
 public class KnowledgeBean {
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getH1() {
+        return h1;
+    }
+
+    public void setH1(String h1) {
+        this.h1 = h1;
+    }
+
+    public String getH2() {
+        return h2;
+    }
+
+    public void setH2(String h2) {
+        this.h2 = h2;
+    }
+
+    public String getH3() {
+        return h3;
+    }
+
+    public void setH3(String h3) {
+        this.h3 = h3;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isTop() {
+        return top;
+    }
+
+    public void setTop(boolean top) {
+        this.top = top;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getNiceDate() {
+        return niceDate;
+    }
+
+    public void setNiceDate(String niceDate) {
+        this.niceDate = niceDate;
+    }
+
+    public String getEnvelopePic() {
+        return envelopePic;
+    }
+
+    public void setEnvelopePic(String envelopePic) {
+        this.envelopePic = envelopePic;
+    }
+
     /**
      * 种类
      */
@@ -31,5 +111,31 @@ public class KnowledgeBean {
      * 内容
      */
     private String content;
+
+    /**
+     * 是否置顶
+     */
+    private boolean top;
+
+    /**
+     * 标签
+     */
+    private String tags;
+
+    /**
+     * 作者
+     */
+    private String author;
+
+
+    /**
+     * 日期
+     */
+    private String niceDate;
+
+    /**
+     * 照片
+     */
+    private String envelopePic;
 
 }
